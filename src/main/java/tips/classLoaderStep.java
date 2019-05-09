28fd0045ca0ee7b7d527510920bfd038dd43f53d
RubyJean <976235684@qq.com>
2019-05-09 16:29:45 +0800

    上传项目，类方法加载顺序

---
+++ src/main/java/tips/classLoaderStep.java
@@ -0,0 +1,41 @@
+package tips;
+
+/**
+ * JAVA中静态块、静态变量加载顺序详解
+ * step: 加载静态代码块 ——>加载匿名函数——>加载构造函数——>加载静态方法
+ * remark:1.常量，按照顺序加载
+ *        2.静态方法，被调用才加载
+ */
+/** 第一步，加载类 **/
+public class classLoaderStep {
+    public static void main(String[] args) {
+        new classLoaderStep();
+        run();
+    }
+    /** 第二步，加载静态代码块 **/
+    static int num = 4;
+    static {
+        System.out.println("=================加载父类静态代码块==================="+num);
+    }
+
+    /** 常量，按照顺序加载 **/
+    int a = 5;
+
+    /** 第三步，加载匿名函数 **/
+    {
+        num += 3;
+        System.out.println("=================加载父类匿名函数==================="+a);
+    }
+
+    /** 第四步，加载构造函数 **/
+    classLoaderStep() {
+        System.out.println("=================加载父类构造函数==================="+a);
+    }
+
+
+    /** 静态方法，被调用才加载 **/
+    static void run()
+    {
+        System.out.println("=================加载父类静态方法===================");
+    }
+}
---
+++ src/main/java/tips/classSonLoaderStep.java
@@ -0,0 +1,39 @@
+package tips;
+
+/**
+ * 继承时，加载静态块、静态变量加载顺序详解
+ * step:加载父类静态代码块(仅一次)——>加载子类静态代码块(仅一次)——>加载父类匿名函数、构造函数——>加载子类匿名函数、构造函数
+ * ---调用--->加载（父类or子类）静态方法
+ * remark:静态代码块永远会执行，且只执行一次
+ */
+public class classSonLoaderStep extends classLoaderStep{
+    public static void main(String[] args) {
+        new classLoaderStep();
+    }
+    /** 第二步，加载静态代码块 **/
+    static int num = 6;
+    static {
+        System.out.println("=================加载子类静态代码块==================="+num);
+    }
+
+    /** 常量，按照顺序加载 **/
+    int a = 7;
+
+    /** 第三步，加载匿名函数 **/
+    {
+        num += 3;
+        System.out.println("=================加载子类匿名函数==================="+a);
+    }
+
+    /** 第四步，加载构造函数 **/
+    classSonLoaderStep() {
+        System.out.println("=================加载子类构造函数==================="+a);
+    }
+
+
+    /** 静态方法，被调用才加载 **/
+    static void run()
+    {
+        System.out.println("=================加载子类静态方法===================");
+    }
+}
