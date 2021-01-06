03bfb08194aede62289834123f02561de9c4dbc7
RubyJean <976235684@qq.com>
2021-01-06 11:47:41 +0800

    类的代码块加载顺序重新理解

--- src/main/java/tips/ClassLoaderStep.java
+++ src/main/java/tips/ClassLoaderStep.java
@@ -2,16 +2,16 @@ package tips;
 
 /**
  * JAVA中静态块、静态变量加载顺序详解
- * step: 加载静态代码块 ——>加载匿名函数——>加载构造函数——>加载静态方法
- * remark:1.常量，按照顺序加载
- *        2.静态方法，被调用才加载
+ * step: 加载静态代码块（无论如何都会被先加载，不管调不调用类，且仅一次）
+ * 静态方法、静态变量属于类
+ * 实例：加载匿名函数——>加载构造函数
  */
 /** 第一步，加载类 **/
 public class ClassLoaderStep {
-//    public static void main(String[] args) {
-//        new ClassLoaderStep();
+    public static void main(String[] args) {
 //        run();
-//    }
+//        new ClassLoaderStep();
+    }
     /** 第二步，加载静态代码块 **/
     static int num = 4;
     static {
